894b888f68693b234f8dc48eb6afe3d461720d1c
Said Mg <saidmghabghab@gmail.com>
2021-05-16 00:25:54 -0400

    fixed the layout in the reviews section

--- src/components/Reviews/index.js
+++ src/components/Reviews/index.js
@@ -6,18 +6,19 @@ import Header from '../Header'
 
 export default function Reviews() {
     return (    
+        <>
     <div style={{backgroundColor:'#1FE1E9'}}>
         <Header color='#1FE1E9'/>
-        <div className="container">
-        <div className="row" style={{paddingTop:'10%'}}>
-            <div className="col-lg-3 col-md-12">
+        <div className="container"  style={{paddingTop:'10%'}}>
+        <div className="row">
+            <div className="col-lg-4 col-md-12">
             <img src={smallSpeaker1} alt="first speaker " width="250"/>
             <img src={smallSpeaker2} alt="second speaker" width="250" style={{marginLeft:'15%'}}/>
             </div>
-            <div className="col-lg-9 col-md-12">
-                <h1 style={{color:'white', margin:'5%'}}>REVIEWS</h1>
-                <div className="row">
-                    <div className="col-4">
+            <div className="col-lg-8 col-md-12">
+                <h1 className="my-5" style={{color:'white'}}>REVIEWS</h1>
+                <div className="row align-items-start">
+                    <div className="col">
                     <span class="fa fa-star m-1"></span>
                     <span class="fa fa-star m-1"></span>
                     <span class="fa fa-star m-1"></span>
@@ -26,7 +27,7 @@ export default function Reviews() {
                         <h5 style={{color:'white'}}>ARTIST</h5>
                         <p>“Love it, it’s the Best.I can’t live without it!”</p>
                     </div>
-                    <div className="col-4">
+                    <div className="col">
                     <span class="fa fa-star m-1"></span>
                     <span class="fa fa-star m-1"></span>
                     <span class="fa fa-star m-1"></span>
@@ -35,7 +36,7 @@ export default function Reviews() {
                         <h5 style={{color:'white'}}>PRODUCER</h5>
                         <p>“Love it, it’s the Best.I can’t live without it!”</p>
                     </div>
-                    <div className="col-4">
+                    <div className="col">
                     <span class="fa fa-star m-1"></span>
                     <span class="fa fa-star m-1"></span>
                     <span class="fa fa-star m-1"></span>
@@ -49,5 +50,6 @@ export default function Reviews() {
         </div>
         </div>
     </div>
+    </>
     )
 }
