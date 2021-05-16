42a342a169cd13bf90977262424c88b6dfe7e297
Said Mg <saidmghabghab@gmail.com>
2021-05-16 00:17:09 -0400

    fixed the reviews page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ export default function Footer() {
         <>
         <div  style={{backgroundColor:'#5E33D1', color:'white'}}>
             <div className="container" style={{paddingTop:'5%', paddingBottom:'5%'}}>
-            <h5>Support@experienceconcerts.co</h5>
+            <h5> <span class="fa fa-envelope"></span> Support@experienceconcerts.co</h5>
             
             <h3 style={{textAlign:'center', marginTop:'5%'}}>EXP|CON</h3>
             <p style={{textAlign:'center'}}>2019 &copy; All Rights Reserved | Speer Technologies Incorporated</p>
